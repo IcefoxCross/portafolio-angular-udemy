40b1ac5262846fab2314f3325752267aee57a5df
IcefoxCross <icefox.990@gmail.com>
2019-07-11 13:18:34 -0300

    Fin Seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,12 +10,26 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     // Leer archivo JSON
     this.http.get('assets/data/data-pagina.json')
       .subscribe((resp: InfoPagina) => {
         this.cargada = true;
         this.info = resp;
+      });
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-html-bc04b.firebaseio.com/equipo.json')
+      .subscribe((resp: any[]) => {
+        this.equipo = resp;
       });
   }
 }
